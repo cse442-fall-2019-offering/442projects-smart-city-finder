da32cf7125873579b83a1b48b4917d0a2960f756
Andrew Fuchs <andrew-fuchs@users.noreply.github.com>
2019-10-10 10:31:50 -0400

    Respond to submit with a placeholder results page
    Resolves: #27

--- server/quiz/views.py
+++ server/quiz/views.py
@@ -6,5 +6,7 @@ from django.shortcuts import render
 
 def quiz_view(request):
     # check if this is a HTTP POST request, then process the quiz data
-    raise Http404()
-    # return render(request, 'quiz/quiz.html')
+    if request.method != 'POST':
+        raise Http404()
+    context = {}
+    return render(request, 'quiz/quiz_results.html', context)
