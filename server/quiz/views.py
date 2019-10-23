c0eca1871a54a0a0563835f2021735946fc878e7
Rishi <chaitanya167@gmail.com>
2019-10-23 17:31:28 -0400

    added function signatures in form views and stubed out their return values

--- server/quiz/views.py
+++ server/quiz/views.py
@@ -4,18 +4,44 @@ from django.shortcuts import render
 
 from time import sleep
 
+'''
+This fn. retrieves the data from quiz form
+    '''
 def get_inputs(request):
-    print(request)
-    
+    if request.method == 'POST':
+        print(request.POST)
+
+    '''
+This fn. is used to check if the input is correct. (i.e., check if the user 
+entered any characters in place of integers, etc)
+    '''
+def validate_inputs(request):
+    pass
+
+    '''
+This fn. cleans up inputs and formats it so the model can run through the data without a problem
+    '''
+def sanitize_inputs():
+    pass  
+
+    '''
+This function creates the ML model
+    '''
 def get_model():
     pass
 
-def sanitize_input():
     '''
-
-    
+We use dummy data with target labels to train the ML model
     '''
+def train_model():
     pass
+
+    ''' 
+We run the ML model on the validated and sanitized inputs to get our results
+    '''
+def inference():
+    pass
+
 
 # Create your views here.
 def quiz_view(request):
@@ -25,6 +51,6 @@ def quiz_view(request):
 
     # FIXME remove this when we have an actual implementation
     sleep(0.25) # pretend that we are actually doing work
-
+ 
     context = {}
     return render(request, 'quiz/quiz_results.html', context)
