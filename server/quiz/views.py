e8d6083006d4df4e5e7b33c05a76b55a0a43225f
Hanming Liu <hanmingl@users.noreply.github.com>
2019-10-23 22:39:52 -0400

    Refactor quiz/views.py. Improved documentation on data processing pipeline.

--- server/quiz/views.py
+++ server/quiz/views.py
@@ -3,43 +3,76 @@ from django.http import Http404
 from django.shortcuts import render
 
 from time import sleep
+import numpy as np
+from mxnet import nd
+from mxnet.gluon import nn
 
-'''
-This fn. retrieves the data from quiz form
+def get_user_input(request):
     '''
-def get_inputs(request):
-    if request.method == 'POST':
-        print(request.POST)
+    Retrieve user inputted data from quiz form.
+    param:
+        request -> A POST request object containing user inputted data as
+        key:value pairs
+    result -> A numpy array of shape (1, 1, 9) with numerical features.
+    '''
+    # if request.method == 'POST':
+    #     print(request.POST)
 
+    input = np.random.randint(0, 1, size=(1, 1, 9))
+    valid = validate_user_input(input)
+    if not valid:
+        #FIXME: Define output when user input is not valid.
+        pass
+
+    result = sanitize_user_input(input)
+    return result
+
+
+def validate_user_input(input):
     '''
-This fn. is used to check if the input is correct. (i.e., check if the user 
-entered any characters in place of integers, etc)
+    Check if the input is correct. (i.e., check if the user
+    entered any characters in place of integers, etc)
     '''
-def validate_inputs(request):
+    #FIXME: Define actual validation process.
+    return True
+
+
+def sanitize_user_input(input):
+    '''
+    Clean up user input so convert it into a format suitable for inference.
+    '''
+    #FIXME: Define actual data sanitization process.
+    return input
+
+
+def retrieve_model():
+    '''
+    Retrieve the multi-layer perceptron (MLP) model for later use.
+    '''
+
+    mlp = nn.Sequential()
+
+    mlp.add(nn.Dense(256, activation="relu"),
+            nn.Dense(128, activation="relu"),
+            nn.Dense(64, activation="relu"),
+            nn.Dense(32, activation="relu"),
+            nn.Dense(9))
+
+    mlp.initialize()
+    return mlp
+
+
+def train_model(model):
+    '''
+    Train the MLP on dummy data.
+    '''
     pass
 
-    '''
-This fn. cleans up inputs and formats it so the model can run through the data without a problem
-    '''
-def sanitize_inputs():
-    pass  
 
+def inference(model, user_input):
     '''
-This function creates the ML model
+    Run the MLP on user input to get prediction.
     '''
-def get_model():
-    pass
-
-    '''
-We use dummy data with target labels to train the ML model
-    '''
-def train_model():
-    pass
-
-    ''' 
-We run the ML model on the validated and sanitized inputs to get our results
-    '''
-def inference():
     pass
 
 
@@ -51,6 +84,6 @@ def quiz_view(request):
 
     # FIXME remove this when we have an actual implementation
     sleep(0.25) # pretend that we are actually doing work
- 
+
     context = {}
     return render(request, 'quiz/quiz_results.html', context)
